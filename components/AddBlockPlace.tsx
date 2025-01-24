02e72569a7c683f589ff4be75dc878ef8ddef88a
Volodymyr Sevastianov <volodymyr@nodon.se>
2025-01-24 13:25:02 +0100

    return: + button position

--- components/AddBlockPlace.tsx
+++ components/AddBlockPlace.tsx
@@ -12,7 +12,7 @@ const AddBlockPlace: React.FC<AddBlockPlaceProps> = ({
   return (
     <button
       onClick={handleAddBlock}
-      className={`w-10 h-10  -mt-5 -mb-5 flex justify-center items-center bg-white hover:bg-sky-200 text-gray-600 border-dashed border-gray-300 border-2 text-xl mb-1 ml-2 ${
+      className={`w-10 h-10  -mt-5 -mb-3 flex justify-center items-center bg-white hover:bg-sky-200 text-gray-600 border-dashed border-gray-300 border-2 text-xl mb-1 ml-2 ${
         position === 'left' ? 'mr-20' : 'ml-20'
       }`}
       title="Click to add block"
